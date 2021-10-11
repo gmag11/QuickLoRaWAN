e61d75a78692d768fb9a157952b4393242e2aca1
Germán Martín <github@gmartin.net>
2021-10-12 00:34:28 +0200

    Move private keys to ignored file

--- src/main.cpp
+++ src/main.cpp
@@ -56,22 +56,26 @@
 # define FILLMEIN (#dont edit this, edit the lines that use FILLMEIN)
 #endif
 
+#if __has_include("keys.h")
+#include "keys.h"
+#else
 // This EUI must be in little-endian format, so least-significant-byte
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
 static const u1_t PROGMEM APPEUI[8] = { FILLMEIN };
-void os_getArtEui (u1_t* buf) { memcpy_P (buf, APPEUI, 8); }
-
 // This should also be in little endian format, see above.
 static const u1_t PROGMEM DEVEUI[8] = { FILLMEIN };
-void os_getDevEui (u1_t* buf) { memcpy_P (buf, DEVEUI, 8); }
-
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is.
 static const u1_t PROGMEM APPKEY[16] = { FILLMEIN };
+#endif
+
+void os_getArtEui (u1_t* buf) { memcpy_P (buf, APPEUI, 8); }
+void os_getDevEui (u1_t* buf) { memcpy_P (buf, DEVEUI, 8); }
 void os_getDevKey (u1_t* buf) { memcpy_P (buf, APPKEY, 16); }
+
 
 static uint8_t mydata[] = "Hello world!!!";
 
